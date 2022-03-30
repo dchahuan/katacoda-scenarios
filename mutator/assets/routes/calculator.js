7439160c858f4c235b57657153b117e0da5807a6
Preston Chao <prestonchao1@gmail.com>
2022-03-30 17:51:12 +0200

    Added helper functions for calculator

--- mutator/assets/routes/calculator.js
+++ mutator/assets/routes/calculator.js
@@ -2,38 +2,51 @@ const KoaRouter = require('koa-router')
 
 const router = new KoaRouter()
 
+function add(num1, num2) {
+    return num1 + num2;
+}
 
 router.get('/add/:num1/:num2', async (ctx) => {
-    
+    ans = add(parseInt(ctx.params.num1), parseInt(ctx.params.num2))
     ctx.body = {
-        'answer': parseInt(ctx.params.num1) + parseInt(ctx.params.num2)
+        'answer': ans
     }
 })
 
+function subtract(num1, num2) {
+    return num1 - num2;
+}
 
-router.post('/substract', async (ctx) => {
-    console.log('aaaaaa')
-    console.log(ctx.request.body)
+router.post('/subtract', async (ctx) => {
     const {num1, num2}  = ctx.request.body;
-    
+    ans = subtract(num1, num2);
     ctx.body = {
-        'answer': num1 - num2
+        'answer': ans
     }
 })
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
 
 router.post('/multiply', async (ctx) => {
     const {num1, num2}  = ctx.request.body;
+    ans = multiply(num1, num2);
     ctx.body = {
-        'answer': num1 * num2
+        'answer': ans
     }
 })
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
 
 router.post('/divide', async (ctx) => {
     const {num1, num2}  = ctx.request.body;
+    ans = divide(num1, num2);
     ctx.body = {
-        'answer': num1 / num2
+        'answer': ans
     }
 })
-
 
 module.exports = router;
