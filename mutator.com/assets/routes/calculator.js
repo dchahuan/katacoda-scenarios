eb78a193f4339aa2b770ecc6138677ee6723273a
dchahuan <d.chahuan@uc.cl>
2022-03-30 15:57:48 +0200

    files

--- mutator.com/assets/routes/calculator.js
+++ mutator.com/assets/routes/calculator.js
@@ -6,13 +6,16 @@ const router = new KoaRouter()
 router.get('/add/:num1/:num2', async (ctx) => {
     
     ctx.body = {
-        'answer': ctx.params.num1 + ctx.params.num2
+        'answer': parseInt(ctx.params.num1) + parseInt(ctx.params.num2)
     }
 })
 
 
 router.post('/substract', async (ctx) => {
+    console.log('aaaaaa')
+    console.log(ctx.request.body)
     const {num1, num2}  = ctx.request.body;
+    
     ctx.body = {
         'answer': num1 - num2
     }
