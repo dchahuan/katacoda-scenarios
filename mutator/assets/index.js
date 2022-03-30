6bbf0b023b555f21e2f7d92d517ee5ceb4d19307
dchahuan <d.chahuan@uc.cl>
2022-03-30 18:07:01 +0200

    todo

--- mutator/assets/index.js
+++ mutator/assets/index.js
@@ -1,18 +1,4 @@
-const Koa = require('koa')
-const koaBody = require('koa-body');
+const app = require('./app');
 
-const app = new Koa()
-
-const calcRouter = require('./routes/calculator')
-const todoRouter = require('./routes/todo.js')
-
-app.use(koaBody({
-    multipart: true,
-    keepExtensions: true,
-}));
-  
-
-app.use(calcRouter.routes())
-app.use(todoRouter.routes())
 
 app.listen(3000)
--- mutator/assets/routes/todo.js
+++ mutator/assets/routes/todo.js
@@ -1,4 +1,4 @@
-const todos = require('../db')
+let todos = require('../db')
 const KoaRouter = require('koa-router')
 
 const router = new KoaRouter()
@@ -21,12 +21,28 @@ router.post('/todos', async (ctx) => {
 
 router.patch('/todos/:id', async (ctx) => {
     const { text } = ctx.request.body;
-    const todo = todos.filter(t => t.id == ctx.params.id)
+    let todo = todos.filter(t => t.id == ctx.params.id)
+    if (todo.length === 0){
+        ctx.status = 404
+        ctx.body = {
+            'message':"error"
+        }
+        return
+    }
+
     todo.text = text
     ctx.body = todos
 })
 
 router.delete('/todos/:id', async (ctx) => {
+    let todo = todos.filter(t => t.id == ctx.params.id)
+    if (todo.length === 0){
+        ctx.status = 404
+        ctx.body = {
+            'message':"error"
+        }
+        return
+    }
     todos = todos.filter(t => t.id !== ctx.params.id)
     ctx.body = todos
 })
